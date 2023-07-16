19b7429c05f34d8d6ce00f55f0c48f77ed5a4f01
Luke Ye <em21119@bristol.ac.uk>
2023-07-16 21:30:43 +0100

    rename

--- dataloading.py
+++ dataloading.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 NUM_WORKERS = os.cpu_count()
 
 
-def from_path(dataset_dir: str, batch_size: int, transform: transforms.Compose, shuffle=True, dataset_type=datasets.ImageFolder, num_workers: int = NUM_WORKERS) -> DataLoader:
+def from_path(dataset_dir: str, batch_size: int, transform: transforms.Compose = transforms.ToTensor(), shuffle=True, dataset_type=datasets.ImageFolder, num_workers: int = NUM_WORKERS) -> DataLoader:
     dataset = dataset_type(dataset_dir, transforms=transform)
     labels = dataset.classes
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=True)
@@ -14,7 +14,7 @@ def from_path(dataset_dir: str, batch_size: int, transform: transforms.Compose, 
     return dataloader, labels
 
 
-def traintest_from_path(trainset_dir: str, testset_dir: str, batch_size: int, transform: transforms.Compose, dataset_type=datasets.ImageFolder, num_workers: int = NUM_WORKERS) -> tuple(DataLoader, DataLoader):
+def traintest_from_path(trainset_dir: str, testset_dir: str, batch_size: int, transform: transforms.Compose = transforms.ToTensor(), dataset_type=datasets.ImageFolder, num_workers: int = NUM_WORKERS) -> tuple(DataLoader, DataLoader):
     if trainset.classes == testset.classes:
         trainset = dataset_type(trainset_dir, transforms=transform)
         testset = dataset_type(testset_dir, transforms=transform)
--- engine.py
+++ engine.py
@@ -1,8 +1,9 @@
 import torch
+import metrics
 import numpy as np
 
 
-def fit(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, optimiser=torch.optim.Adam, loss_func=torch.nn.MSELoss(), epochs=100, lr=0.01, device="cpu"):
+def fit(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, loss_func=torch.nn.MSELoss(), optimiser=torch.optim.Adam, epochs=100, lr=0.01, device="cpu"):
     model.to(device)
     optimiser = optimiser(model.parameters(), lr=lr)
 
@@ -12,15 +13,16 @@ def fit(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, optimis
         model.train()
 
         batch_loss = 0
-        for batch, (x, y) in enumerate(dataloader):
+        for (x, y) in dataloader:
             x, y = x.to(device), y.to(device)
+
             y_pred = model(x)
             optimiser.zero_grad()
             loss = loss_func(y_pred, y)
             batch_loss += loss
             loss.backward()
             optimiser.step()
-
+ 
         epoch_loss = batch_loss / len(dataloader)
         losses[epoch] = epoch_loss.item()
 
