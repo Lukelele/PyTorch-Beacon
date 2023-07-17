cb6d7897dfac8172dc4483918be91061758e41cf
Luke Ye <em21119@bristol.ac.uk>
2023-07-17 19:18:10 +0100

    utils save/load

---
+++ README.md
@@ -0,0 +1,7 @@
+PyTorch-Beacon
+
+A lightweight library with the purpose of simplifying training and testing models in PyTorch
+Github Link: https://github.com/Lukelele/PyTorch-Beacon
+
+
+Installation: pip install pytorch-beacon
--- beacon/utils.py
+++ beacon/utils.py
@@ -1,4 +1,18 @@
+from pathlib import Path
 import torch
+from collections import OrderedDict
 
 
+def save_model(model: torch.nn.Module, save_directory: str, model_name: str):
+    target_directory = Path(save_directory)
+    target_directory.mkdir(parents=True, exist_ok=True)
 
+    assert model_name.endswith(".pth") or model_name.endswith(".pt")
+    model_path = target_directory / model_name
+
+    torch.save(model.state_dict(), model_path)
+
+
+def load_model(model: torch.nn.Module, model_state_path: str):
+    model_state = torch.load(model_state_path)
+    model.load_state_dict(model_state)
