6b674095076d3158df5c3f99c26d5a7bf64eb066
Luke Ye <lukelele2001@gmail.com>
2023-07-25 12:52:46 +0100

    fixed bug with Module.py

--- beacon/module.py
+++ beacon/module.py
@@ -11,9 +11,9 @@ class Module(torch.nn.Module):
         super().__init__()
         
         
-    def compile(self, optimiser: torch.optim.Optimizer = torch.optim.Adam, learning_rate=0.1, loss_function=torch.nn.CrossEntropyLoss, accuracy_function=metrics.categorical_accuracy, device: str = "cpu", optimisations=False):
-        self.loss_function = loss_function()
-        self.optimiser = optimiser(self.parameters(), learning_rate)
+    def compile(self, optimiser: torch.optim.Optimizer=torch.optim.Adam, learning_rate=0.1, loss_function=torch.nn.CrossEntropyLoss, accuracy_function=metrics.categorical_accuracy, device: str = "cpu", optimisations=False):
+        self.loss_function = loss_function
+        self.optimiser = optimiser
         self.learning_rate = learning_rate
         self.accuracy_function = accuracy_function
         self.device = device
@@ -24,6 +24,10 @@ class Module(torch.nn.Module):
             
     def fit(self, dataloader: torch.utils.data.DataLoader, epochs=10):
         self.to(self.device)
+        
+        loss_function = self.loss_function()
+        optimiser = self.optimiser(self.parameters(), self.learning_rate)
+        accuracy_function = self.accuracy_function
 
         losses = np.zeros(epochs)
         accuracies = np.zeros(epochs)
@@ -39,14 +43,14 @@ class Module(torch.nn.Module):
 
                 y_pred = self(x)
 
-                loss = self.loss_function(y_pred, y)
+                loss = loss_function(y_pred, y)
                 batch_loss += loss
-                accuracy = self.accuracy_function(y_pred, y)
+                accuracy = accuracy_function(y_pred, y)
                 batch_accuracy += accuracy
 
-                self.optimiser.zero_grad()
+                optimiser.zero_grad()
                 loss.backward()
-                self.optimiser.step()
+                optimiser.step()
     
             epoch_loss = batch_loss / len(dataloader)
             epoch_accuracy = batch_accuracy / len(dataloader)
@@ -59,6 +63,9 @@ class Module(torch.nn.Module):
     def evaluate(self, dataloader: torch.utils.data.DataLoader):
         self.to(self.device)
         self.eval()
+        
+        loss_function = self.loss_function()
+        accuracy_function = self.accuracy_function
 
         loss = 0
         accuracy = 0
@@ -68,8 +75,8 @@ class Module(torch.nn.Module):
             for (x, y) in dataloader:
                 x, y = x.to(self.device), y.to(self.device)
                 y_pred = self(x)
-                loss += self.loss_function(y_pred, y)
-                accuracy += self.accuracy_function(y_pred, y)
+                loss += loss_function(y_pred, y)
+                accuracy += accuracy_function(y_pred, y)
 
             loss /= len(dataloader)
             accuracy /= len(dataloader)
@@ -81,91 +88,17 @@ class Module(torch.nn.Module):
 class Sequential(Module):
     def __init__(self, *args):
         super().__init__()
-        if len(args) == 1 and isinstance(args[0], OrderedDict):
-            for key, module in args[0].items():
-                self.add_module(key, module)
-        else:
-            for idx, module in enumerate(args):
-                self.add_module(str(idx), module)
+        
+        for i, module in enumerate(args):
+            self.add_module(str(i), module)
+        
                 
     def forward(self, inputs):
         for module in self:
             inputs = module(inputs)
         return inputs
     
-    def __iter__(self) -> Iterator[Module]:
+    
+    def __iter__(self):
         return iter(self._modules.values())
-
-
-
-
-# class Sequential(torch.nn.Sequential):
-#     def __init__(self, *layers: torch.nn.Module):
-#         super().__init__(*layers)
-    
-    
-#     def compile(self, optimiser: torch.optim.Optimizer = torch.optim.Adam, learning_rate=0.1, loss_function=torch.nn.CrossEntropyLoss, accuracy_function=metrics.categorical_accuracy, device: str = "cpu", optimisations=False):
-#         self.loss_function = loss_function()
-#         self.optimiser = optimiser(self.parameters(), learning_rate)
-#         self.learning_rate = learning_rate
-#         self.accuracy_function = accuracy_function
-#         self.device = device
-        
-#         if optimisations == True:
-#             self = torch.compile(self)
-            
-            
-#     def fit(self, dataloader: torch.utils.data.DataLoader, epochs=10):
-#         self.to(self.device)
-
-#         losses = np.zeros(epochs)
-#         accuracies = np.zeros(epochs)
-
-#         self.train()
-
-#         for epoch in tqdm(range(epochs)):
-#             batch_loss = 0
-#             batch_accuracy = 0
-
-#             for (x, y) in dataloader:
-#                 x, y = x.to(self.device), y.to(self.device)
-
-#                 y_pred = self(x)
-
-#                 loss = self.loss_function(y_pred, y)
-#                 batch_loss += loss
-#                 accuracy = self.accuracy_function(y_pred, y)
-#                 batch_accuracy += accuracy
-
-#                 self.optimiser.zero_grad()
-#                 loss.backward()
-#                 self.optimiser.step()
-    
-#             epoch_loss = batch_loss / len(dataloader)
-#             epoch_accuracy = batch_accuracy / len(dataloader)
-#             losses[epoch] = epoch_loss.item()
-#             accuracies[epoch] = epoch_accuracy.item()
-
-#         return (losses, accuracies)
-    
-    
-#     def evaluate(self, dataloader: torch.utils.data.DataLoader):
-#         self.to(self.device)
-#         self.eval()
-
-#         loss = 0
-#         accuracy = 0
-
-#         with torch.inference_mode():
-#             loss = 0
-#             for (x, y) in dataloader:
-#                 x, y = x.to(self.device), y.to(self.device)
-#                 y_pred = self(x)
-#                 loss += self.loss_function(y_pred, y)
-#                 accuracy += self.accuracy_function(y_pred, y)
-
-#             loss /= len(dataloader)
-#             accuracy /= len(dataloader)
-
-#         return loss.item(), accuracy.item()
     
